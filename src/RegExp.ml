678fb612e1a21900a3a4c9c2c884399c0bca3b9a
Ian Treyball <ian.treyball@columbia.edu>
2018-02-16 15:06:42 -0500

    add mostly finished OCaml RegExp implementation

--- src/RegExp.ml
+++ src/RegExp.ml
@@ -1,11 +1,49 @@
+exception TODO of string
+
 module RegExp = struct
   type 'a regexp =
-    | Zero                               (* The empty language         -- L(Zero)  = ∅              *)
-    | One                                (* The empty string, epsilon  -- L(One)   = {ε}            *)
-    | Lit  of 'a                         (* Literal, single symbol     -- L(σ)     = {σ}, for σ ∈ Σ *)
-    | Plus of ('a regexp) * ('a regexp)  (* Concatenation              -- L(α · β) = L(α) · L(β)    *)
-    | Mult of ('a regexp) * ('a regexp)  (* Plus, union, or            -- L(α | β) = L(α) ∪ L(β)    *)
-    | Star of ('a regexp)                (* Kleene star, repetition    -- L(α⋆)    = L(α)⋆          *)
+    | Zero                               (* The empty language             -- L(Zero)  = ∅              *)
+    | One                                (* The empty string, epsilon      -- L(One)   = {ε}            *)
+    | Lit  of 'a                         (* Literal, single symbol         -- L(σ)     = {σ}, for σ ∈ Σ *)
+    | Plus of ('a regexp) * ('a regexp)  (* Multiplication, Concatenation  -- L(α · β) = L(α) · L(β)    *)
+    | Mult of ('a regexp) * ('a regexp)  (* Plus, union, or                -- L(α | β) = L(α) ∪ L(β)    *)
+    | Star of ('a regexp)                (* Kleene star, repetition        -- L(α⋆)    = L(α)⋆          *)
+  (*
+  -- Concatentation is associative          (LM)N = L(MN)
+  -- ε is identity for concatenation
+  -- ∅ is annihilation for concatenation    ∅L = L∅ = ∅
+  -- distributes over +     L(M+N) = LM + LN
+  --                        (M+N)L = ML + NL
+  *)
+  let rec mult (r1 : 'a regexp) (r2 : 'a regexp) : 'a regexp = match (r1, r2) with
+      (_, Zero) -> Zero                                        (* -- Annihilation for mult is ∅ *)
+    | (Zero, _) -> Zero                                        (* -- Annihilation for mult is ∅ *)
+    | (One, b)  -> b                                           (* -- Identity for mult is ε *)
+    | (a, One)  -> a                                           (* -- Identity for mult is ε *)
+    | (a, b)    -> match a with
+                      Mult (a1, a2) -> Mult (a1, (mult a2 b))
+                    | _             -> Mult (a, b)
+  (*
+  -- Union is commutative L+M = M+L
+  --          associative (L+M)+N = L+(M+N)
+  --          idempotent  L + L = L
+  *)
+  let rec plus (r1 : 'a regexp) (r2 : 'a regexp) : 'a regexp = match (r1, r2) with
+        (a, Zero) -> a     (* Identity for plus is ∅ *)
+      | (Zero, b) -> b     (* Identity for plus is ∅ *)
+      | (_, _) -> raise (TODO "finish implementation")
+  let rec star (r : 'a regexp) : 'a regexp = match r with
+      Zero   -> One     (* -- ∅⋆ ≈ ε *)
+    | One    -> One     (* -- ε⋆ ≈ ε *)
+    | Star a -> star a  (* -- recursively apply idempotence L⋆⋆ ≈ L⋆ *)
+    | a      -> Star a
+  let rec normalize = function
+        Zero        -> Zero
+      | One         -> One
+      | Lit  c      -> Lit c
+      | Plus (a, b) -> plus (normalize a) (normalize b)
+      | Mult (a, b) -> mult (normalize a) (normalize b)
+      | Star a      -> star (normalize a)
   let rec nullable = function
        Zero        -> false
      | One         -> true
@@ -14,4 +52,35 @@ module RegExp = struct
      | Mult (a, b) -> (nullable a) && (nullable b)
      | Star a      -> true
   let constant (r : 'a regexp) : 'a regexp = if nullable r then One else Zero
+  (* Check if the the regular expression, r, produces a finite language.
+     This is accomplished by finding the normal form of r
+     (which removes extra Kleene star operations) and then checking if
+     that normal form still has a Kleene star. *)
+  let finite (r : 'a regexp) : bool =
+        let rec finite' = function
+            Zero        -> true
+          | One         -> true
+          | Lit  _      -> true
+          | Plus (a, b) -> (finite' a) && (finite' b)
+          | Mult (a, b) -> (finite' a) && (finite' b)
+          | Star _      -> false
+        in finite' (normalize r)
+  let infinite (r : 'a regexp) : bool = not (finite r)
+  (* Brzozowski derivative with respect to σ ∈ Σ *)
+  let rec derivative (r : 'a regexp) (s : 'a) : 'a regexp = match r with
+      Zero        -> Zero
+    | One         -> Zero
+    | Lit c       -> if c = s then One else Zero
+    | Plus (a, b) -> plus (derivative a s) (derivative b s)
+    | Mult (a, b) -> plus (mult (derivative a s) b) (mult (constant a) (derivative b s))
+    | Star a      -> mult (derivative a s) (star a)
+  let derivative' (r : 'a regexp) (word : 'a list) = List.fold_left derivative r word
+  (* can be written point-free as:
+   let derivative' = List.fold_left derivative
+  *)
+  let rec matches (r : 'a regexp) (word : 'a list) = match r with
+      Zero -> false
+    | a    -> match word with
+                []        -> constant a = One
+              | (x :: xs) -> matches (derivative a x) xs
 end
