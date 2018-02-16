810ef943382f77a668b21590e2ceba6887675d5d
Lalksy <ler2161@columbia.edu>
2018-02-16 14:07:52 -0500

    test accept string and transitions

--- src/dfa.c
+++ src/dfa.c
@@ -27,27 +27,26 @@ struct transition{ //embed transition function into states via links
 struct dfa_t{
   /*
     Q is a finite set of states.
-    
     ∑ is a finite set of symbols called the alphabet.
-    
     δ is the transition function where δ: Q × ∑ → Q
-    
     q0 is the initial state from where any input is processed (q0 ∈ Q).
-    
     F is a set of final state/states of Q (F ⊆ Q).
   */
   struct dfa_state *states;//points to collection of states
   int nstates;
   char *alphabet;
   int nsym;
-  struct dfa_state *init;
-  struct dfa_state **final;
+  int init;//by state id
+  int *final;//by state id
   int nfin;
 };
 
 
 //DFA functions
-struct dfa_t construct(struct dfa_state * states, int nstates, char * alphabet, int nsym, struct dfa_state *init, struct dfa_state **final, int nfin){
+
+//constructor
+//TODO: check final and init are in states
+struct dfa_t construct(struct dfa_state * states, int nstates, char * alphabet, int nsym, int init, int *final, int nfin){
   struct dfa_t self;
   self.states = states;
   self.nstates = nstates;
@@ -61,6 +60,7 @@ struct dfa_t construct(struct dfa_state * states, int nstates, char * alphabet, 
 }
 
 // free memory
+// assume final and alphabet were malloc'd
 void destruct(struct dfa_t d){
   if(d.states) {
     free(d.states);
@@ -81,12 +81,15 @@ struct dfa_state * makeStates(int *ids, int len, char * alpha, int nsym){
     sts[i].links = malloc(nsym*sizeof(struct transition));
     for(int j = 0; j < nsym; j++){
       sts[i].links[j].input = alpha[i];
+      sts[i].nlinks = nsym;
     }
   }
 
   return sts;
 }
 
+// helper function
+//returns pointer to state with id if it is in the dfa
 struct dfa_state * hasState(struct dfa_t dfa, int id){
   for(int i = 0; i < dfa.nstates; i++){
     if(id == dfa.states[i].ID){
@@ -96,6 +99,8 @@ struct dfa_state * hasState(struct dfa_t dfa, int id){
   return NULL;
 }
 
+
+//helper function returns true if sym is in the dfa's alphabet
 int hasSymbol(struct dfa_t dfa, char sym){
   for(int i = 0; i < dfa.nsym; i++){
     if(sym == dfa.alphabet[i]){
@@ -105,6 +110,9 @@ int hasSymbol(struct dfa_t dfa, char sym){
   return 0;
 }
 
+//adds a transition from 'from' to 'to' on input symbol sym
+// if sym is not in the alphbet, returns 0
+// on success, returns 1
 int link(struct dfa_t dfa, struct dfa_state* from, struct dfa_state* to, char sym){
   if(hasState(dfa, from->ID) && hasState(dfa, to->ID) && hasSymbol(dfa, sym)){
     for(int i = 0; i < dfa.nsym; i++){
@@ -117,29 +125,49 @@ int link(struct dfa_t dfa, struct dfa_state* from, struct dfa_state* to, char sy
   return 0;
 }
 
-struct dfa_state *transition(struct dfa_state s, char input){ return NULL;}
+// follows transition from s on input
+// returns a pointer to the resulting state
+struct dfa_state *transition(struct dfa_state *s, char input){
+  for(int i = 0; i < s->nlinks; i++){
+    if(s->links[i].input == input){
+      return s->links[i].next;
+    }
+  }
+  return s;
+}
 
 //takes list of characters and returns a result state
 struct dfa_state *evaluate(struct dfa_t dfa, char *input){
-  
-  for(int i = 0;i < strlen(input); i++){
-    //do transition for each char
-    ;
+
+  struct dfa_state * nxt = hasState(dfa, dfa.init);
+  for(int i = 0; i < strlen(input); i++){
+    printf("transition from %i on %c\n", nxt->ID, input[i]);
+    nxt = transition(nxt, input[i]);
   }
-  return NULL;
+  printf("ended on %i\n", nxt->ID);
+  return nxt;
 }
 
 
-
+//Simluates input on dfa
+//returns 1 if accepted
+//0 if not accepted
 int accept(struct dfa_t dfa, char *input){
   struct dfa_state * check = evaluate(dfa, input);
+  for(int i = 0; i < dfa.nfin; i++){
+    if(dfa.final[i] == check->ID){
+      return 1;
+    }
+  }
   return 0;    
 }
 
 
 int main(){
   struct dfa_t test;
-  char alpha[] = {'0','1'};
+  char *alpha = malloc(2*sizeof(char));
+  alpha[0] = '0';
+  alpha[1] = '1';
   int ids[] = {3,2,1};
   struct dfa_state * sts = makeStates(ids, sizeof(ids)/sizeof(int), alpha, 2);
   test.states = sts;
@@ -151,7 +179,35 @@ int main(){
   
   printf("Has 12: %i\n", hasState(test, 12));
 
-  link(test, hasState(test, 3), hasState(test,2), '0');
+  printf("Test2---------\n");
 
+  int *fin = malloc(sizeof(int));
+  fin[0] = 1;
+
+  struct dfa_t test2 = construct(sts, 3, alpha, 2, 3, fin, 1);
+
+  for(int i = 0; i < sizeof(ids)/sizeof(int); i++){
+    printf("Has st %i: %i\n", ids[i], hasState(test, ids[i]));
+  }
+
+  printf("Has 12: %i\n", hasState(test, 12));
+
+  link(test2, hasState(test2, 3), hasState(test2, 2), '0');
+  link(test2, hasState(test2, 2), hasState(test2, 1), '1');
+  link(test2, hasState(test2, 1), hasState(test2, 2), '1');
+  
+  printf("3->2 on 0: %i\n", (transition(hasState(test2, 3), '0'))->ID);
+  printf("3 on 1: %i\n", (transition(hasState(test2, 3), '1'))->ID);
+  printf("2 on 0: %i\n", (transition(hasState(test2, 2), '0'))->ID);
+  printf("2->1 on 1: %i\n", (transition(hasState(test2, 1), '1'))->ID);
+  printf("1 on 0: %i\n", (transition(hasState(test2, 1), '0'))->ID);
+  printf("1->2 on 1: %i\n", (transition(hasState(test2, 1), '1'))->ID);
+
+  printf("Accepts 01?: %i\n", accept(test2, "01"));
+  printf("Accepts 00?: %i\n", accept(test2, "00"));
+  printf("Accepts xx?: %i", accept(test2, "xx"));
+  
+  destruct(test2);
+  
   return 0;
 }
