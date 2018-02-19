692a296215516b2c272a6c1d62a83c8887ef56cf
Ian Treyball <ian.treyball@columbia.edu>
2018-02-18 22:18:46 -0500

    add some more RE tests

--- src/tests/regexp_tests/test_regexp.ml
+++ src/tests/regexp_tests/test_regexp.ml
@@ -28,4 +28,14 @@ let r5 = RegExp.Star(r2);;
 let b5 = RegExp.isZero r5;;
 assert(b5 == false);;
 
+let xs = List.map (fun x -> RegExp.Lit x) [1; 2; 3; 4; 5; 6; 5; 4; 3; 2; 1; 0; 1; 1;1;7;8;9];;
+let ys = List.map (fun x -> RegExp.Lit x) [0; 1; 2; 3; 4; 5; 6; 7; 8; 9];;
+let digit = List.fold_left (fun acc b -> RegExp.plus acc b) RegExp.Zero ys;;
+(* assert the algebraic properties properly reduce
+   the regex (1 | 2 | 3 | 4 | 5 | 6 | 5 | 4 | 3 | 2 | 1 | 0 | 1 | 1 | 1 | 7 | 8 | 9 )
+   to        (0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9)
+*)
+assert (digit = List.fold_left (fun acc b -> RegExp.plus acc b) RegExp.Zero xs)
+assert (digit = RegExp.Plus (RegExp.Lit 0, RegExp.Plus (RegExp.Lit 1, RegExp.Plus (RegExp.Lit 2, RegExp.Plus (RegExp.Lit 3, RegExp.Plus (RegExp.Lit 4, RegExp.Plus (RegExp.Lit 5, RegExp.Plus (RegExp.Lit 6, RegExp.Plus (RegExp.Lit 7, RegExp.Plus (RegExp.Lit 8, RegExp.Lit 9)))))))))
 
+assert (RegExp.matches (RegExp.star digit) [1;2;3])
