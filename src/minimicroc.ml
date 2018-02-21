18d3a7f4586048270f8f75bba65608c43ba95b4d
Ian Treyball <ian.treyball@columbia.edu>
2018-02-21 12:05:53 -0500

    add simple test

--- src/minimicroc.ml
+++ src/minimicroc.ml
@@ -1,7 +1,8 @@
 let () =
-  let usage_msg = "usage: ./microc.native [file.mc]" in
-  let channel = ref stdin in
-  Arg.parse [] (fun file -> channel := open_in file) usage_msg;
-  let lexbuf = Lexing.from_channel !channel in
-  let ast = Parser.program Scanner.token lexbuf in
-  print_string (Ast.string_of_program ast)
+  let usage_msg = "usage: ./microc.native [file.mc]"
+  in let channel = ref stdin
+  in Arg.parse [] (fun file -> channel := open_in file) usage_msg;
+
+  let lexbuf = Lexing.from_channel !channel
+  in let ast = Parser.program Scanner.token lexbuf
+  in print_string (Ast.string_of_program ast)
