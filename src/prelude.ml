4cc99f5c8b2c58201a737cdde94c920705f78a12
Ian Treyball <ian.treyball@columbia.edu>
2018-02-21 11:31:49 -0500

    int_to_nat boolean blindness

--- src/prelude.ml
+++ src/prelude.ml
@@ -8,12 +8,11 @@ module Prelude = struct
   let rec nat_to_int = function
       Zero   -> 0
     | Succ n -> 1 + (nat_to_int n)
-  let rec int_to_nat (i : int) : nat option =
-      if (i < 0)
-      then None
-      else if (i = 0)
-           then Some Zero
-           else match int_to_nat (i - 1) with
-                  None   -> None
-                | Some a -> Some (Succ a)
+  let rec int_to_nat (i : int) : nat option = match i with
+        0 -> Some Zero
+      | n -> if (i < 0)
+             then None
+             else match int_to_nat (i - 1) with
+                    None   -> None
+                  | Some a -> Some (Succ a)
 end
