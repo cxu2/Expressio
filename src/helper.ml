a8e14be538f63a706edb2883d16ed2cbfa1674c9
Ian Treyball <ian.treyball@columbia.edu>
2018-02-21 11:22:35 -0500

    add basic natural numbers (Peano)

--- src/helper.ml
+++ src/helper.ml
@@ -3,4 +3,17 @@ module Prelude = struct
   let const (x : 'a) (y : 'b) : 'a = x
   let id (x : 'a) = x
   exception TODO of string
+
+  type nat = Zero | Succ of nat
+  let rec nat_to_int = function
+      Zero   -> 0
+    | Succ n -> 1 + (nat_to_int n)
+  let rec int_to_nat (i : int) : nat option =
+      if (i < 0)
+      then None
+      else if (i = 0)
+           then Some Zero
+           else match int_to_nat (i - 1) with
+                  None   -> None
+                | Some a -> Some (Succ a)
 end
