07848c7bea935d2f50d9ea9c86042ecf49fc69d3
Ian Treyball <ian.treyball@columbia.edu>
2018-02-22 01:19:52 -0500

    add fn composition to Prelude

--- src/prelude.ml
+++ src/prelude.ml
@@ -1,9 +1,17 @@
 module Prelude = struct
+  (* sum types *)
   type ('a, 'b) either = Left of 'a | Right of 'b
+  (* the constant function *)
   let const (x : 'a) (y : 'b) : 'a = x
+  (* the identity function *)
   let id (x : 'a) = x
+  (* function composition "f after g" *)
+  let after (f : 'b -> 'c) (g : 'a -> 'b) (x : 'a) = f (g x)
+  (* given a function of one argument (being a pair), turn it into a function of two arguments *)
   let curry (f : (('a * 'b) -> 'c)) (x : 'a) (y : 'b) : 'c = f (x, y)
+  (* given a function of two arguments, turn it into a function of one argument (a pair) *)
   let uncurry (f : 'a -> 'b -> 'c) (p : ('a * 'b)) : 'c = f (fst p) (snd p)
+
   let first (triple : 'a * 'b * 'c) : 'a = match triple with
     (a , _ , _) -> a
   let second (triple : 'a * 'b * 'c) : 'b = match triple with
@@ -13,6 +21,7 @@ module Prelude = struct
   exception TODO of string
   module StringMap = Map.Make(String)
 
+  (* Implement the Natural numbers Peano style *)
   type nat = Zero | Succ of nat
   let rec nat_to_int = function
       Zero   -> 0
