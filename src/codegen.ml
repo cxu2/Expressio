6b73f381461cae809310e04e34c65b11a6231e92
Lalksy <ler2161@columbia.edu>
2018-03-23 15:30:55 -0400

    llvm struct and string type successfully used

--- src/codegen.ml
+++ src/codegen.ml
@@ -24,7 +24,7 @@ open Prelude
    throws an exception if something is wrong. *)
 let translate (globals, dfas, functions) =
   let context    = L.global_context ()
-  (* Add types to the context so we can use them in our LLVM code *)
+   (* Add types to the context so we can use them in our LLVM code *)
   in let i32_t      = L.i32_type    context
      and i8_t       = L.i8_type     context
      and i1_t       = L.i1_type     context
@@ -34,14 +34,17 @@ let translate (globals, dfas, functions) =
      generate actual code *)
      and the_module = L.create_module context "Expressio"
   (* Convert MicroC types to LLVM types *)
+  in let dfa_t =
+      let types = Array.of_list [i32_t; L.pointer_type i8_t; i32_t; i32_t; L.pointer_type i32_t; i32_t; L.pointer_type (L.pointer_type i32_t)] in
+      L.struct_type context types
   in let ltype_of_typ = function
       A.TInt    -> i32_t
     | A.TBool   -> i1_t
     | A.TChar   -> raise (Prelude.TODO "LLVM Char")
     | A.TUnit   -> void_t
     | A.TRegexp -> raise (Prelude.TODO "LLVM RegExp")
-    | A.TString -> raise (Prelude.TODO "LLVM String")
-    | A.TDFA    -> raise (Prelude.TODO "LLVM DFA")
+    | A.TString -> L.pointer_type i8_t
+   (*) | A.TDFA    -> dfa_t*)
 
   (* Declare each global variable; remember its value in a map *)
   in let global_vars =
@@ -49,10 +52,15 @@ let translate (globals, dfas, functions) =
       let init = L.const_int (ltype_of_typ t) 0
       in StringMap.add n (L.define_global n init the_module) m in
     List.fold_left global_var StringMap.empty globals in
+(*)
+  let dfa_decls = 
+    let dfa_decl m (t, n) =
+      let init = L.
+*)
 
   let printf_t = L.var_arg_function_type i32_t [| L.pointer_type i8_t |]
   in let printf_func = L.declare_function "printf" printf_t the_module
-
+  
   (* Define each function (arguments and return type) so we can
    * define it's body and call it later *)
   in let function_decls =
