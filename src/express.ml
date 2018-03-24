b4952a17216737696ef0e4561ce861c1dc8bd0db
David Han <dth2126@columbia.edu>
2018-03-24 14:22:25 -0400

    commented express for cleaning

--- src/express.ml
+++ src/express.ml
@@ -1,18 +1,19 @@
 (* Top-level of the MicroC compiler: scan & parse the input,
    check the resulting AST and generate an SAST from it, generate LLVM IR,
    and dump the module *)
-open Prelude
+(* open Prelude *)
 
-type action = Ast | Sast | LLVM_IR | Compile
+(* type action = Ast | Sast | LLVM_IR | Compile *)
+type action = Ast | Compile
 
 let () =
   let action = ref Compile in
   let set_action a () = action := a in
   let speclist = [
     ("-a", Arg.Unit (set_action Ast),     "Print the AST");
-    ("-s", Arg.Unit (set_action Sast),    "Print the SAST");
+    (* ("-s", Arg.Unit (set_action Sast),    "Print the SAST");
     ("-l", Arg.Unit (set_action LLVM_IR), "Print the generated LLVM IR");
-    ("-c", Arg.Unit (set_action Compile), "Check and print the generated LLVM IR (default)");
+    ("-c", Arg.Unit (set_action Compile), "Check and print the generated LLVM IR (default)"); *)
   ] in
   let usage_msg = "usage: ./express.native [-a|-s|-l|-c] [file.mc]" in
   let channel = ref stdin in
@@ -21,6 +22,7 @@ let () =
   let ast = Parser.program Scanner.token lexbuf in
     match !action with
       Ast     ->                                   print_string (Ast.string_of_program ast)
+    | Compile ->                                   ()
     (* | Sast    -> let sast = Semant.check ast    in print_string (Sast.string_of_sprogram sast)
     | LLVM_IR ->                                   print_string (Llvm.string_of_llmodule (Codegen.translate sast))
     | Compile -> let m = Codegen.translate sast in Llvm_analysis.assert_valid_module m;
