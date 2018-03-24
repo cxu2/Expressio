ccf57e141d979efa4a757635421c538fa6b30a45
Chengtian <cxu2@bates.edu>
2018-03-24 15:20:57 -0400

    travis back to ubuntu

--- travis-ci.sh
+++ travis-ci.sh
@@ -1,9 +1,10 @@
 # install ocaml from apt
-brew install --with-toolchain llvm
-brew install ocaml
-brew install opam
+wget https://raw.github.com/ocaml/opam/master/shell/opam_installer.sh -O - | sh -s /usr/local/bin/
+sudo apt-get update
+sudo apt-get install -y ocaml m4 llvm
 opam init
 opam update
 opam upgrade
 opam install llvm ocamlfind
+eval `opam config env`
 
