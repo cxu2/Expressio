484a6fb562b6cecfbfb9661568ec69a5d9ad0ebc
Chengtian <cxu2@bates.edu>
2018-03-24 15:25:58 -0400

    update opam

--- travis-ci.sh
+++ travis-ci.sh
@@ -1,7 +1,6 @@
 # install ocaml from apt
-wget https://raw.github.com/ocaml/opam/master/shell/opam_installer.sh -O - | sh -s /usr/local/bin/
-sudo apt-get update
-sudo apt-get install -y ocaml m4 llvm
+apt-get update opam
+apt-get install -y ocaml m4 llvm
 opam init
 opam update
 opam upgrade
