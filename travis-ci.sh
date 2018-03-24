cd67661524789e96d232c04bf0e7e1767dc516c1
Chengtian <cxu2@bates.edu>
2018-03-24 15:31:21 -0400

    opam......

--- travis-ci.sh
+++ travis-ci.sh
@@ -1,9 +1,7 @@
 # install ocaml from apt
-apt-get install -y ocaml m4 llvm
-apt-get update
+sudo apt-get install -y ocaml m4 llvm
+sudo apt-get update
 opam init
-opam update
-opam upgrade
 opam install llvm ocamlfind
 eval `opam config env`
 
