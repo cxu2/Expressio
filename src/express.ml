6eecabccf32d637d535f0b3b6ecd24a1956ae00a
Ian Treyball <ian.treyball@columbia.edu>
2018-03-24 13:10:46 -0400

    allow for different parameters in top level

--- src/express.ml
+++ src/express.ml
@@ -20,14 +20,9 @@ let () =
   Arg.parse speclist (fun filename -> channel := open_in filename) usage_msg;
   let lexbuf = Lexing.from_channel !channel in
   let ast = Parser.program Scanner.token lexbuf in
-  match !action with
-    Ast -> print_string (Ast.string_of_program ast)
-  | _   -> raise (Prelude.TODO "express.ml")
-  (* | _ -> let sast = Semant.check ast in
     match !action with
-      Ast     -> ()
-    | Sast    -> print_string (Sast.string_of_sprogram sast) *)
-    (* | LLVM_IR -> print_string (Llvm.string_of_llmodule (Codegen.translate sast))
-    | Compile -> let m = Codegen.translate sast in
-	Llvm_analysis.assert_valid_module m;
-	print_string (Llvm.string_of_llmodule m) *)
+      Ast     ->                                   print_string (Ast.string_of_program ast)
+    | Sast    -> let sast = Semant.check ast    in print_string (Sast.string_of_sprogram sast)
+    | LLVM_IR ->                                   print_string (Llvm.string_of_llmodule (Codegen.translate sast))
+    | Compile -> let m = Codegen.translate sast in Llvm_analysis.assert_valid_module m;
+	                                                 print_string (Llvm.string_of_llmodule m)
