93524754187d5fd2f55f3a27baac372d9d9b6677
Ian Treyball <ian.treyball@columbia.edu>
2018-03-26 20:34:05 -0400

    improve express.ml

--- src/express.ml
+++ src/express.ml
@@ -1,9 +1,9 @@
 (* Top-level of the MicroC compiler: scan & parse the input,
    check the resulting AST and generate an SAST from it, generate LLVM IR,
    and dump the module *)
-(* open Prelude *)
-type action = Ast | Sast | Compile
-(* type action = Ast | Sast | LLVM_IR | Compile *)
+open Prelude
+
+type action = Ast | Sast | LLVM_IR | Compile
 
 let () =
   let action = ref Compile in
@@ -11,7 +11,7 @@ let () =
   let speclist = [
     ("-a", Arg.Unit (set_action Ast),     "Print the AST");
     ("-s", Arg.Unit (set_action Sast),    "Print the SAST");
-    (* ("-l", Arg.Unit (set_action LLVM_IR), "Print the generated LLVM IR"); *)
+    ("-l", Arg.Unit (set_action LLVM_IR), "Print the generated LLVM IR");
     ("-c", Arg.Unit (set_action Compile), "Check and print the generated LLVM IR (default)");
   ] in
   let usage_msg = "usage: ./express.native [-a|-s|-l|-c] [file.mc]" in
@@ -21,8 +21,7 @@ let () =
   let ast = Parser.program Scanner.token lexbuf in
     match !action with
       Ast     ->                                   print_string (Ast.string_of_program ast)
-    | Compile ->                                   ()
     | Sast    -> let sast = Semant.check ast    in print_string (Sast.string_of_sprogram sast)
-    (* | LLVM_IR -> let sast = Semant.check ast    in print_string (Llvm.string_of_llmodule (Codegen.translate sast))
-    | Compile -> let m = Codegen.translate sast in Llvm_analysis.assert_valid_module m;
-	                                                 print_string (Llvm.string_of_llmodule m) *)
+    | LLVM_IR -> raise (Prelude.TODO "express.ml LLVM_IR") (* let sast = Semant.check ast    in print_string (Llvm.string_of_llmodule (Codegen.translate sast)) *)
+    | Compile -> raise (Prelude.TODO "express.ml Compile") (* let m = Codegen.translate sast in Llvm_analysis.assert_valid_module m;
+                                                              print_string (Llvm.string_of_llmodule m) *)
