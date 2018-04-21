89189f8054b680557488d968321336d3f292c6c3
Chengtian <cxu2@bates.edu>
2018-04-21 11:54:24 -0400

    [travis-ci] use opam after .travis-opam

--- travis-ci.sh
+++ travis-ci.sh
@@ -1,10 +1,5 @@
-export OPAM_VERSION=1.2.2
-export OPAM_PACKAGES='ocamlfind llvm.3.6'
-
 # install ocaml from apt
-sudo apt-get update
-sudo apt-get install -y ocaml m4 llvm
-opam init
+sudo apt-get install -y llvm
 opam install llvm.3.6 ocamlfind
 eval `opam config env`
 
