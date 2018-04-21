e3a7294c00f1d120ed276e2fcf6527df6f856a5c
Chengtian <cxu2@bates.edu>
2018-04-21 10:41:04 -0400

    [travis] fix opam version

--- travis-ci.sh
+++ travis-ci.sh
@@ -2,8 +2,9 @@ export OPAM_VERSION=1.2.2
 export OPAM_PACKAGES='ocamlfind llvm.3.6'
 
 # install ocaml from apt
+yes Yes | wget https://raw.github.com/ocaml/opam/master/shell/opam_installer.sh -O - | sh -s /usr/local/bin/
+sudo apt-get update
 sudo apt-get install -y ocaml m4 llvm
-sudo apt-get install -y opam=1.2.2
 opam init
 opam install llvm.3.6 ocamlfind
 eval `opam config env`
