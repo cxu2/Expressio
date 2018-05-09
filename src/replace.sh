5d0da49f35463ef88a6218b137195e097e313b54
David Han <dth2126@columbia.edu>
2018-05-09 06:09:24 -0400

    more edits to replace.sh

--- src/replace.sh
+++ src/replace.sh
@@ -1,12 +1,25 @@
 #!/bin/sh
+
+#./replace.sh FILENAME stdlib.mk stdlib2.mk
 touch newfile.xp
+grep = $1 
+t=$?
+echo $t
+once=0
 while read p; do
 	echo $p | grep "main" 2> /dev/null
 	if [ $? -eq 0 ]; then
+		echo $p >> newfile.xp
   		cat $2 >> newfile.xp
-  		echo $p >> newfile.xp
   	else
-        echo $p >> newfile.xp
+		echo $p | grep "=" 2> /dev/null
+		if [ $? -eq 0 ] && [ $once -eq 0 ]; then
+			cat $3 >> newfile.xp
+			echo $p >> newfile.xp
+			once=1
+		else
+		echo $p >> newfile.xp
+		fi
     fi
 done <$1
 #b=$(basename "$1")
--- src/stdlib.mk
+++ src/stdlib.mk
@@ -7,18 +7,3 @@ regexp ALPHA;
 regexp ALPHANUMERIC
 regexp DIGITS;
 regexp ASCII;
-LOWER = lit 'a' | lit 'b' | lit 'c' | lit 'd' | lit 'e' | lit 'f' | lit 'g' | lit 'h' | lit 'i' | lit 'j' | lit 'k' | lit 'l' | lit 'm' | lit 'n' | lit 'o' | lit 'p' | lit 'q' | lit 'r' | lit 's' | lit 't' | lit 'u' | lit 'v' | lit 'w' | lit 'x' | lit 'y' | lit 'z';
-UPPER = lit 'A' | lit 'B' | lit 'C' | lit 'D' | lit 'E' | lit 'F' | lit 'G' | lit 'H' | lit 'I' | lit 'J' | lit 'K' | lit 'L' | lit 'M' | lit 'N' | lit 'O' | lit 'P' | lit 'Q' | lit 'R' | lit 'S' | lit 'T' | lit 'U' | lit 'V' | lit 'W' | lit 'X' | lit 'Y' | lit 'Z';
-NUMERICS = lit '0' | lit '1' | lit '2' | lit '3' | lit '4' | lit '5' | lit '6' | lit '7' | lit '8' | lit '9';
-ALPHA = alphaLower | alphaUpper;
-ALPHANUMERIC = alpha | numeric;
-DIGITS = numeric **;
-ASCII = lit ' ' | lit '!' | lit '"' | lit '#' | lit '$' | lit '%' | lit '&' | lit '\'' | lit '(' | lit ')' | lit '*' | lit '+' | lit ',' | lit '-' | lit '.' | lit '/' | digits | lit ':' | lit ';' | lit '<' | lit '=' | lit '>' | lit '?' | lit '@' | alphaUpper | lit '[' | lit '\' | lit ']' | lit '^' | lit '_' | lit '`' | alphaLower | lit '{' | lit '|' | lit '}' | lit '~' | lit 'DEL' 
-
-
-isNumeric : (s : string) -> bool {
-  return (digits matches s);
-}
-isAscii : (s : string) -> bool {
-	return (ascii matches s);
-}
